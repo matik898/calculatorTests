a1d7e0023dcaa2036199a0477ee6c0dcc8a5cb0e
Mateusz Iskrowicz <matik898@oi2.pl>
2020-06-29 18:29:08 +0200

    tests

---
+++ MainWindowTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void CheckToFloatFunctionTest()
+        {
+            Assert.AreEqual(2.5, MainWindow.ToFloat("2,5"));
+        }
+
+        [TestMethod()]
+        public void GetInitResultTest()
+        {
+            Assert.AreEqual(0, MainWindow.GetInitResult());
+        }
+    }
+}
